33849f9bcde1349f11b8b68b116155992fae56ae
spades <usv@intelins.ru>
2020-02-11 07:53:40 +0300

    Initial commit

---
+++ modules/core/src/com/company/userprovidertestcase/core/userprovider/UserProvider.java
@@ -0,0 +1,38 @@
+package com.company.userprovidertestcase.core.userprovider;
+
+
+import com.haulmont.addon.bproc.entity.ExecutionData;
+import com.haulmont.addon.bproc.provider.UserListProvider;
+import com.haulmont.addon.bproc.service.BprocRuntimeService;
+import com.haulmont.cuba.security.entity.User;
+import org.slf4j.Logger;
+import org.springframework.stereotype.Component;
+
+import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component("user_provider")
+public class UserProvider implements UserListProvider {
+
+    @Inject
+    private BprocRuntimeService bprocRuntimeService;
+
+    @Inject
+    private Logger log;
+
+    @Override
+    public List<User> get(String executionId) {
+        List<ExecutionData> list = bprocRuntimeService.createExecutionDataQuery().executionId(executionId).list();
+        if (list.size() == 0) {
+            log.debug("ERROR: Execution not found, ID: " + executionId);
+        }
+        return new ArrayList<>();
+    }
+}
+
+
+
+
+
+
